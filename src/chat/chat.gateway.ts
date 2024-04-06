3c56c1107a65dae19ac054f66492f763c65edd16
alevante <alejandrovelasquezmolina@gmail.com>
2024-04-05 23:27:07 -0500

    feat: adding socket io and create socket connection server

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,7 +1,20 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { OnModuleInit } from '@nestjs/common';
+import { Server, Socket } from 'socket.io';
+
 import { ChatService } from './chat.service';
 
 @WebSocketGateway()
-export class ChatGateway {
+export class ChatGateway implements OnModuleInit {
+
+  @WebSocketServer()
+  public server: Server;
+  
   constructor(private readonly chatService: ChatService) {}
+
+  onModuleInit() {
+    this.server.on('connection', (socket: Socket) => {
+      console.log('Cliente conectado');
+    })
+  }
 }
